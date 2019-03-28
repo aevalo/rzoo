5bbce5242ce707d264c60b64ec86baa13f6c3336
Arttu Valo <arttu.valo@gmail.com>
2019-03-28 22:53:50 +0200

    Changed project to multi-project build.

---
+++ librzoo/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// externing crate for test-only use
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
---
+++ rzoo/src/main.rs
@@ -0,0 +1,47 @@
+#[macro_use]
+extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rzoo", about = "An example of StructOpt usage.")]
+#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+struct Opt {
+    /// Activate debug mode
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+    /// Set speed
+    #[structopt(short = "s", long = "speed", default_value = "42")]
+    speed: f64,
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
+}
+
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+  let opt = Opt::from_args();
+  println!("{:?}", opt);
+}
+
+// externing crate for test-only use
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
